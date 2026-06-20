ab7db449fa64215ff4dba83e61d3e50bf927f6d1
Ivan Nahornyi <ivan.nahornyi.93@gmail.com>
2026-06-21 00:22:23 +0300

    test: update test for TargetClosedError

--- src/test/java/ui/performance/simulations/regression/TargetClosedErrorRecover.java
+++ src/test/java/ui/performance/simulations/regression/TargetClosedErrorRecover.java
@@ -10,9 +10,7 @@ import io.gatling.javaapi.core.Simulation;
 import static io.gatling.javaapi.core.CoreDsl.*;
 
 @SuppressWarnings("unused")
-public class ClosedPageRecover extends Simulation {
-
-    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(FailedSimulation.class);
+public class TargetClosedErrorRecover extends Simulation {
 
     ProtocolBuilder browserProtocol = BrowserDsl
             .gatlingBrowser()
@@ -24,34 +22,37 @@ public class ClosedPageRecover extends Simulation {
 
     ScenarioBuilder mainScenario = scenario("test")
             .exec(
+                    BrowserDsl.browserSessionFunction((page, browserSession) -> browserSession.updateBrowserSession(browserSession.getGatlingSession().set("testValue", "testValue"))),
                     BrowserDsl.browserAction("SuccessAction").open("https://gatling.io/"),
                     pause(1),
-                    exec(session -> session.set("testValue", "testValue")),
-                    BrowserDsl.browserAction("FailedAction").executeFlow((page, browserSession) -> {
-                        page.close();
-                        page.check("#main-content");
-                        return browserSession;
-                    }),
-                    pause(1),
-                    BrowserDsl.browserAction("SuccessAction").open("https://gatling.io/"),
                     BrowserDsl.browserAction("FailedAction").executeFlow((page, browserSession) -> {
                         page.context().browser().close();
                         page.check("#main-content");
                         return browserSession;
                     }),
+                    pause(1),
                     BrowserDsl.browserAction("SuccessAction").open("https://gatling.io/"),
-                    exec(session -> {
-                        log.warn(session.getString("testValue"));
-                        return session;
+                    BrowserDsl.browserAction("FailedAction").executeFlow((page, browserSession) -> {
+                        page.context().close();
+                        page.check("#main-content");
+                        return browserSession;
                     }),
+                    pause(1),
+                    BrowserDsl.browserAction("SuccessAction").open("https://gatling.io/"),
+                    BrowserDsl.browserAction("FailedAction").executeFlow((page, browserSession) -> {
+                        page.close();
+                        page.check("#main-content");
+                        return browserSession;
+                    }),
+                    BrowserDsl.browserAction("SuccessAction").open("https://gatling.io/"),
                     crashLoadGeneratorIf(session -> "testValue lost", session -> !session.contains("testValue"))
             );
 
 
     {
         setUp(mainScenario.injectOpen(atOnceUsers(1))).assertions(
-                global().successfulRequests().count().is(3L),
-                global().failedRequests().count().is(2L)
+                global().successfulRequests().count().is(4L),
+                global().failedRequests().count().is(3L)
         );
     }
 }
